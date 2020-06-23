177525df81d7197b05e47718147ad4f24b23ff11
JoaquinMelladoQ <mellado.joaquin@gmail.com>
2020-06-22 22:29:47 -0400

    creating a petition to consume a jsonplaceholder

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -4,10 +4,12 @@ const router = Router();
 const movies = require('../sample.json');
 const _ = require('underscore');
 
+//get method
 router.get('/', (req, res) => {
      res.json(movies);
 });
 
+//post method
 router.post('/', (req, res) => {
      const { title, director, year, rating } = req.body;
      if(title && director && year && rating){
@@ -21,6 +23,26 @@ router.post('/', (req, res) => {
      }
 });
 
+//put method
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const { title, director, year, rating } = req.body;
+      if(title && director && year && rating){
+          _.each(movies, (movie, i) => {
+              if(movie.id == id) {
+                  movie.title = title;
+                  movie.director = director;
+                  movie.year = year;
+                  movie.rating = rating;
+              }
+          });
+          res.json(movies);
+      } else {
+          res.status(500).json({error: 'There was an error'});
+      }
+});
+
+//delete method
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     _.each(movies, (movie, i) => {
